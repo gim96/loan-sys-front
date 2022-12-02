adbb93e14b3240546d71203fbba74802d7bdc2bb
gim96 <gimkelum@gmail.com>
2022-12-03 01:30:05 +0530

    df

--- src/pages/db/server.js
+++ src/pages/db/server.js
@@ -1,5 +1,5 @@
 export function getSource() {
   // const url = "https://www.inivac-erp.xyz";
-  const url = "http://localhost:9000/api/v0.1";
+  const url = "http://138.2.229.192:9000/api/v0.1";
   return url;
 };
